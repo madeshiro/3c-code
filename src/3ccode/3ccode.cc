72586a973ca0d77400fef28fb84530ba56d26b81
Rin Bdlt <rin.bdlt@outlook.com>
2023-03-27 10:40:10 +0200

    IDE Initial commit

---
+++ src/3ccode/3ccode.cc
@@ -0,0 +1,3 @@
+//
+// Created by madeshiro on 27/03/23.
+//
---
+++ src/main.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using std::cout, std::endl;
+
+int main(int argc [[maybe_unused]], char** argv [[maybe_unused]])
+{
+    return 0;
+}
