25b6ad68d52939f0e2ea8b0abe9b6b960c4b7a49
Rin B <rin.bdlt@outlook.com>
2023-04-06 23:44:15 +0200

    write license header in pixelmap.hh

--- include/code3c/pixelmap.hh
+++ include/code3c/pixelmap.hh
@@ -1,3 +1,19 @@
+/*
+ * 3C-CODE Library
+ * Copyright (C) 2023 - Rin "madeshiro" Baudelet
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this
+ * program. If not, see <https://www.gnu.org/licenses/>.
+ */
 #ifndef HH_LIB_PIXELMAP
 #define HH_LIB_PIXELMAP
 #include "3ccodelib.hh"
