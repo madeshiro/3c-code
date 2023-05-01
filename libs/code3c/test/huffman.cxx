103ba49a3e717dd489489ea80678df53cc2459c6
Rin Baudelet <rin.bdlt@outlook.com>
2023-05-01 21:43:49 +0200

    Create Huffman code's test file

---
+++ libs/code3c/test/huffman.cxx
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <code3c/huffman.hh>
+
+using code3c::HuffmanTree;
+using code3c::HuffmanTable;
+using code3c::HTFile;
+
+typedef int (*TestFunction)(void); /* NOLINT */
+typedef struct /* NOLINT */
+{
+    const char* name;
+    TestFunction func;
+    uint32_t id;
+    int exit_code;
+} testFunctionMapEntry;
+
+static testFunctionMapEntry registeredFunctionEntries[] = {
+
+};
+
+int test_huffman(int argc [[maybe_unused]], char** argv [[maybe_unused]])
+{
+    uint32_t status(0u), pass(0),
+             found(sizeof(registeredFunctionEntries)/sizeof(testFunctionMapEntry));
+
+    std::cout << "Running Huffman code tests..." << std::endl;
+    std::cout << "Found " << found << " test(s) to run" << std::endl;
+
+    for (testFunctionMapEntry &entry : registeredFunctionEntries)
+    {
+        std::cout << "test " << entry.name << "... ";
+        entry.exit_code = entry.func();
+        if (entry.exit_code != 0)
+        {
+            std::cout << "FAIL with return code " << entry.exit_code << std::endl;
+            status |= (0x1 << entry.id);
+        }
+        else
+        {
+            pass++;
+            std::cout << "OK" << std::endl;
+        }
+    }
+
+    std::cout << pass << "/" << found << " test(s) passed" << std::endl;
+    return (int) status;
+}
